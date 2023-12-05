12d86a6beb48b096af532209e5759f7e96062ab1
Fozaan khan <fozaan@Fozaans-MacBook-Pro.local>
2023-12-05 13:56:14 -0500

    home screen

--- TaskharborApp/src/main/java/controllers/HomeController.java
+++ TaskharborApp/src/main/java/controllers/HomeController.java
@@ -1,15 +1,34 @@
 package controllers;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import library.App;
 
-public class HomeController {
+public class HomeController implements Initializable{
 
-    @FXML
-    private void switchToPrimary() throws IOException {
-        App.setRoot("primary");
+    private void onLoginclicked(ActionEvent event) {
+        try {
+            App.setRoot("login");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
-    
+
+    private void onSignupclicked(ActionEvent event) {
+        try {
+            App.setRoot("signup");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        throw new UnsupportedOperationException("Unimplemented method 'initialize'");
+    }
 
 }
