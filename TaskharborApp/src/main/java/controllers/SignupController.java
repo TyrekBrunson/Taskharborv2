f44ad9344ae8cbc808753472199f624025df264a
Fozaan khan <fozaan@Fozaans-MacBook-Pro.local>
2023-12-06 06:40:26 -0500

    added the signup page, needs functionality

---
+++ TaskharborApp/src/main/java/controllers/SignupController.java
@@ -0,0 +1,31 @@
+package controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+import library.App;
+
+public class SignupController {
+
+    @FXML
+    private TextField firstNameField;
+
+    @FXML
+    private TextField lastNameField;
+
+    @FXML
+    private TextField usernameField;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    private Text signupMessage;
+
+    @FXML
+    private void onSignupClicked(ActionEvent event) {
+        signupMessage.setText("Signup button clicked!");
+    }
+}
