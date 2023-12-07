62c8def68cc478fd8dbe0d5903b5d014a8584be4
Meet Patel <meet@email.sc.edu>
2023-12-07 10:15:15 -0500

    ProjectController

--- TaskharborApp/src/main/java/controllers/ProjectController.java
+++ TaskharborApp/src/main/java/controllers/ProjectController.java
@@ -1,5 +1,34 @@
 package controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+import javafx.event.ActionEvent;
+import javafx.geometry.Insets;
+import library.App;
+import model.UiFacade;
+
 public class ProjectController {
-    
+
+    @FXML
+    private GridPane gridPane;
+
+    @FXML
+    private Label projectLabel;
+
+    @FXML
+    private Button switchToLoginButton;
+
+    @FXML
+    private void initialize() {
+        // Initialize your controller, if needed
+    }
+
+    @FXML
+    private void addProject(ActionEvent event) {
+        // Handle the button click to switch to the login view
+        UiFacade.addProject(null, null, null);
+    }
 }
